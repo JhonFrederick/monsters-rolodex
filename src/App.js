3b1641f9d7db625225e45a72190a0ebe70340c3d
Proyecto <mena.jhon@correounivalle.edu.co>
2020-12-01 21:55:06 -0500

    Add: Card component

--- src/App.js
+++ src/App.js
@@ -21,10 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name="Jhon"><h1>Jhon</h1></CardList>
-        {
-          this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
-        }
+        <CardList monsters={this.state.monsters}/>
       </div>
     );
   }
